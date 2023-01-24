4dcc33ead33c5b64b6ac02a65ca4a01e1ecb5616
neiliscoding <neil.coder@gmail.com>
2023-01-24 20:11:03 +0100

    Update ToDoList.vue
    can now filter out completed items

--- src/components/ToDoList.vue
+++ src/components/ToDoList.vue
@@ -4,7 +4,7 @@
       <h1 class="mt-4 mb-4">Todo List</h1>
       <!-- <p>{{ todos }}</p> -->
       <ul class="list-group" >
-        <li class="list-group-item" v-for="(todo, index) in todos" :key="todo.name"  v-bind:class="{active: ! todo.done}">
+        <li class="list-group-item" v-for="(todo, index) in todos.filter(determineTodoShow)" :key="todo.name"  v-bind:class="{active: ! todo.done}">
           <div class="form-check">
             <input class="form-check-input" type="checkbox" v-bind:id="'formCheck-1' + index" v-model="todo.done" /><label class="form-check-label" for="formCheck-1">{{ todo.text }}</label>
           </div>
@@ -20,7 +20,7 @@
         </div>
         </form>
       <div class="form-check ps-4 ms-3">
-        <input class="form-check-input" type="checkbox" id="formCheck-4" checked /><label class="form-check-label" for="formCheck-4">Show completed items?</label>
+        <input class="form-check-input" type="checkbox" id="formCheck-4" v-model="showDone" /><label class="form-check-label" for="formCheck-4">Show completed items?</label>
       </div>
     </div>
 </template>
@@ -41,6 +41,10 @@
         event.preventDefault();
         this.$data.todos.push({text:this.input, done:false});
         this.input = "";
+      },
+      determineTodoShow (todo) {
+          if(this.$data.showDone) return true;
+          return !todo.done;
       }
     },
     computed: {
